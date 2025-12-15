df893443ac409861762592f0744ef92b558fd36b
ivanaalice <ivana.alice@ti.ukdw.ac.id>
2025-12-15 19:51:36 +0700

    Update Lagiii

--- App.py
+++ App.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import os
-from LoginPages import login_page
-from SignUpPages import signup_page
 
-st.set_page_config(page_title="NeuvaMoneda", layout="wide")
+st.set_page_config(
+    page_title="NeuvaMoneda",
+    layout="wide"
+)
 
 def load_css():
     css_file = "styles.css"
@@ -13,34 +14,32 @@ def load_css():
 
 load_css()
 
-if "page" not in st.session_state:
-    st.session_state.page = "home"
-
 left, center, right = st.columns([1, 2, 1])
 
 with center:
     st.image("LogoNuevaMoneda.png", width=400)
-
     st.markdown("""
-    <p style="text-align:center; font-size:20px; font-style:italic;">
-        “Money is a tool. Used properly it makes something beautiful,
+    <p style="
+        text-align:center;
+        font-size:20px;
+        font-style:italic;
+        color:#dddddd;
+        margin-top:15px;
+    ">
+        “Money is a tool. Used properly it makes something beautiful,  
         used wrong it makes a mess.”
     </p>
     """, unsafe_allow_html=True)
 
+    st.write("")
+
+    # BUTTONS
     col1, col2 = st.columns(2)
 
     with col1:
-        if st.button("Login"):
-            st.session_state.page = "login"
-            st.rerun()
+        if st.button("Login", use_container_width=True):
+            st.switch_page("pages/LoginPages.py")
 
     with col2:
-        if st.button("Sign Up"):
-            st.session_state.page = "signup"
-            st.rerun()
-
-if st.session_state.page == "login":
-    login_page()
-elif st.session_state.page == "signup":
-    signup_page()
+        if st.button("Sign Up", use_container_width=True):
+            st.switch_page("pages/SignUpPages.py")
