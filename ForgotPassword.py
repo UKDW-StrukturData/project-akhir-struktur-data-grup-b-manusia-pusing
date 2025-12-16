eb25a2ce1f57561425f7680ff230bc01b0cec01f
Rafael-Andromeda <rafael.andromeda@ti.ukdw.ac.id>
2025-12-16 20:31:26 +0700

    Update lagiii

--- ForgotPassword.py
+++ ForgotPassword.py
@@ -4,15 +4,27 @@ from database import update_password
 def forgot_password_page():
     st.markdown("""
         <style>
+            .stApp {
+                background: radial-gradient(ellipse at center, #1b2735 0%, #090a0f 100%);
+                color: #e0f7fa;
+            }
             .title {
                 text-align: center;
-                color: #ff9800;
-                font-size: 2.2em;
-                margin-bottom: 30px;
+                color: #00bcd4;
+                font-size: 2.5em;
+                font-weight: bold;
+                margin-top: 20px;
+                margin-bottom: 40px;
+                text-transform: uppercase;
+                letter-spacing: 3px;
+                text-shadow: 0 0 15px rgba(0, 188, 212, 0.8);
             }
         </style>
     """, unsafe_allow_html=True)
 
+    a, b, c = st.columns(3)
+    with b:
+        st.image("LogoNuevaMoneda.png", width=750)
     st.markdown('<div class="title">Reset Password</div>', unsafe_allow_html=True)
 
     username = st.text_input("Username")
